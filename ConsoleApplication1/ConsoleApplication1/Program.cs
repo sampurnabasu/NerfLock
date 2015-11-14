768ac5e322cc3aae9a076fab36d7cefc396b19ed
sampurnabasu <sampurna.basu23@gmail.com>
2015-11-14 15:04:49 -0800

    Tim Cook

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -5,6 +5,9 @@ using System.Net.Http;
 using System.Web;
 using System.IO;
 using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CSHttpClientSample
 {
@@ -26,14 +29,30 @@ namespace CSHttpClientSample
    
     static class Program
     {
+        private static string s1;
+        private static string s2;
+
         static void Main()
         {
-            MakeRequest(); ;
+            Task taskA = Task.Factory.StartNew(() => MakeRequest(0, "http://diabetesinsider.com/wp-content/uploads/2014/11/bill-gates-1024x688.jpg"));
+            taskA.Wait();
+            Console.WriteLine("taskA has completed.");
+
+            Task taskB = Task.Factory.StartNew(() => MakeRequest(1, "http://thefilmstage.com/wp-content/uploads/2011/10/SteveJobsBook.jpg"));
+            taskB.Wait();
+            Console.WriteLine("taskB has completed.");
+
+            if (taskB.IsCompleted && taskA.IsCompleted)
+                MakeRequest2();
+            else
+                Console.WriteLine("Timed out.");
+
+           
             Console.WriteLine("Hit ENTER to exit...");
             Console.ReadLine();
         }
 
-        static async void MakeRequest()
+        static async void MakeRequest(int image, String url)
         {
             var client = new HttpClient();
             var queryString = HttpUtility.ParseQueryString(string.Empty);
@@ -53,7 +72,7 @@ namespace CSHttpClientSample
             HttpResponseMessage response;
 
             // Request body
-            byte[] byteData = Encoding.UTF8.GetBytes("{\"url\":\"http://diabetesinsider.com/wp-content/uploads/2014/11/bill-gates-1024x688.jpg\"}");
+            byte[] byteData = Encoding.UTF8.GetBytes("{\"url\":\""+url+ "\"}");
 
             using (var content = new ByteArrayContent(byteData))
             {
@@ -61,9 +80,60 @@ namespace CSHttpClientSample
                 response = await client.PostAsync(uri, content);
                 String JSON = await response.Content.ReadAsStringAsync();
                 FacialRec facialRec = new FacialRec(JSON);
+          
                 String faceID = facialRec.faceId;
-                Console.WriteLine(faceID);
                 
+                if (image == 0)
+                {
+                    s1 = faceID;
+                    
+                }
+                else
+                {
+                    s2 = faceID;
+                }
+                Console.WriteLine(faceID);
+                Debug.WriteLine(faceID);
+ 
+               
+            }
+
+        }
+        static async void MakeRequest2()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request parameters
+            queryString["analyzesFaceLandmarks"] = "false";
+            queryString["analyzesAge"] = "false";
+            queryString["analyzesGender"] = "false";
+            queryString["analyzesHeadPose"] = "false";
+
+            // Request headers
+            //client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "5017563ebb2d4070b52e16d3869f2a7b");
+
+            var uri = "https://api.projectoxford.ai/face/v0/verifications?" + queryString;
+
+            HttpResponseMessage response;
+
+            String build = "{" +
+                    "\"faceId1\":" + "\"" + s1 + "\"" + "," +
+                    "\"faceId2\":" + "\"" + s2 + "\"" +
+                            "}";
+            Console.WriteLine(build);
+
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes(build);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
+                String JSON = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JSON);
+                Debug.WriteLine(JSON);
             }
 
         }
